f06f462a49156b3d0853f423c2c01a4a26f51055
unknown <dyomin.dyominalexander2011@yandex.ru>
2020-08-26 15:38:18 +0300

    Team is added

--- script/script.js
+++ script/script.js
@@ -15,7 +15,65 @@ function DropDownElements()
     };
 }
 
+function ReviewList()
+{
+    let view_list = document.getElementById('section-reviews').getElementsByClassName('view')[0];
+    let buttons = document.getElementById('section-reviews').getElementsByClassName('btn');
+    let reviews = view_list.getElementsByClassName('review');
+
+    function PlayAnimation()
+    {
+        let duration = 500;
+        for (let i = 0; i < 2; ++i) {
+            reviews[i].classList.add("review--animation");
+        }
+        setTimeout(() => {
+            for (let i = 0; i < 2; ++i)
+                reviews[i].classList.remove('review--animation');
+        }, duration);
+    }
+
+    buttons[1].addEventListener('click', btn => {
+        PlayAnimation();
+        let head = reviews[0].innerHTML;
+        for (let i = 0; i < reviews.length - 1; ++i)
+            reviews[i].innerHTML = reviews[i + 1].innerHTML;
+        reviews[reviews.length - 1].innerHTML = head;
+    });
+    buttons[0].addEventListener('click', btn => {
+        PlayAnimation();
+        let head = reviews[reviews.length - 1].innerHTML;
+        for (let i = reviews.length - 1; i > 0; --i)
+            reviews[i].innerHTML = reviews[i - 1].innerHTML;
+        reviews[0].innerHTML = head;
+    });
+}
+
+function MemberList()
+{
+    let member_list = document.getElementById('section-team').getElementsByClassName('members-list')[0];
+    let members_avatars = member_list.getElementsByClassName('members__avatars')[0];
+    let members = member_list.getElementsByClassName('member--hidden');
+    for (let i = 0; i < members.length; ++i) {
+        let member_av = members[i].getElementsByClassName('member__av--hidden')[0];
+        member_av.classList.add('member__av');
+
+        let img = document.createElement('img');
+        img.src = member_av.src;
+        img.classList.add('member__av--faded');
+        members_avatars.appendChild(img);
+    }
+
+    members_avatars.children[1].classList.remove('member__av--faded');
+    members[1].classList.remove('member--hidden');
+    members[1].classList.add('member');
+
+    let img = document.createElement('img');
+}
+
 function OnLoadFunction()
 {
     DropDownElements();
+    ReviewList();
+    MemberList();
 }
